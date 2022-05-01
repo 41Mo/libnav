ad7edb7cf7948160d2e50b84d2590d5de56135b9
Alex Molchanov <amol4anov@mail.ru>
2022-05-01 17:27:05 +0300

    float constants

--- include/constants.h
+++ include/constants.h
@@ -1,7 +1,7 @@
 #ifndef constants_h__
 #define constants_h__
 // Earth constants
-const float R{6378245.0};
-const float U{0.0000726}; // ((Pi * Wspd_earth)) / 180 degree
-const float G{9.81};
+const float R{6378245.0f};
+const float U{0.0000726f}; // ((Pi * Wspd_earth)) / 180 degree
+const float G{9.81f};
 #endif
