3cbcb24de6bdab58c37f9fe2ebeb6a5ed62ecafd
Alex Molchanov <amol4anov@mail.ru>
2022-05-18 20:24:20 +0300

    add nav_alg imput structure

---
+++ include/input_structure.h
@@ -0,0 +1,17 @@
+#include <optional>
+#include "math.hpp"
+struct D_GNSS {
+	std::optional<matrix::Vector2f> pos{std::nullopt};
+	std::optional<matrix::Vector3f> vel{std::nullopt};
+};
+
+struct D_IMU {
+	std::optional<matrix::Vector3f> acc{std::nullopt};
+	std::optional<matrix::Vector3f> gyr{std::nullopt};
+};
+
+struct D_IN
+{
+	D_IMU imu;
+	D_GNSS gnss;
+};
