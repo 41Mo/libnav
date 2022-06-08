855a5642edd59ef6066237d917ca1a3a79eb562c
Alex Molchanov <amol4anov@mail.ru>
2022-06-08 11:24:39 +0300

    fix corr f32 to std

--- src/nav_cor.cpp
+++ src/nav_cor.cpp
@@ -11,10 +11,10 @@ void Nav_correction::set_time_gnss_corr(float Time)
     exit(-2);
   }
   T = Time;
-  w_s = (2 * M_PIf32) / T;
+  w_s = (2 * M_PIf) / T;
   g_c[0] = 1.75f * w_s;
-  g_c[1] = ((2.15f * powf32(w_s, 2)) / (G / R)) - 1;
-  g_c[2] = (powf32(w_s, 3) / (G / R)) - 1.75f * w_s;
+  g_c[1] = ((2.15f * std::pow(w_s, 2.0f)) / (G / R)) - 1;
+  g_c[2] = (std::pow(w_s, 3.0f) / (G / R)) - 1.75f * w_s;
 }
 
 void Nav_correction::on_off_gnss_corr(bool Mode)
