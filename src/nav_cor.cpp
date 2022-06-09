245bbd0a1144f2831b1385a071e307119beb3e16
Alex Molchanov <amol4anov@mail.ru>
2022-06-09 22:38:51 +0300

    fix M_PI

--- src/nav_cor.cpp
+++ src/nav_cor.cpp
@@ -11,7 +11,7 @@ void Nav_correction::set_time_gnss_corr(float Time)
     exit(-2);
   }
   T = Time;
-  w_s = (2 * M_PIf) / T;
+  w_s = (2.0f * static_cast<float>(M_PI)) / T;
   g_c[0] = 1.75f * w_s;
   g_c[1] = ((2.15f * std::pow(w_s, 2.0f)) / (G / R)) - 1;
   g_c[2] = (std::pow(w_s, 3.0f) / (G / R)) - 1.75f * w_s;
